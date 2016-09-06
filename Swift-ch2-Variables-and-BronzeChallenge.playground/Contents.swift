6cc3339959b2a5526ee55194050a1f65262ef604
Nicholas Becker <nicholas.becker@d25s70nugq18m.oc.edu>
2016-09-05 20:12:27 -0500

    ch 1-3 and partway through ch 4

---
+++ Swift-ch2-Variables-and-BronzeChallenge.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+var unemployment = 0.05     // percent of unemployment
+let townDescription = "\(townName) has a population of \(population), \(unemployment*100)% unemployment, and \(numberOfStoplights) stoplights."
+print(townDescription)
